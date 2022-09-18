f5b139089e94edc202569c4058cc6c2bc0500e4d
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-18 22:59:44 +0600

    Created a Node class for tree travarsing

---
+++ treesBasic.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node(int val)
+    {
+        this->data = val;
+        this->left = this->right = NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
