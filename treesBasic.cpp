0073b55472e2213d488867b7ef33b6d1ca9c21a2
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-18 23:09:57 +0600

    Tested the Tree Code

--- treesBasic.cpp
+++ treesBasic.cpp
@@ -61,7 +61,23 @@ void printPreorder(Node *node)
     printPreorder(node->right);
 }
 
+/* Driver program to test above functions*/
 int main()
 {
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "\nPreorder traversal of binary tree is \n";
+    printPreorder(root);
+
+    cout << "\nInorder traversal of binary tree is \n";
+    printInorder(root);
+
+    cout << "\nPostorder traversal of binary tree is \n";
+    printPostorder(root);
+
     return 0;
 }
